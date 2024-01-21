7622e40c195e0fdcb5516b413bc84094b0a01a4d
PRVOFFCL <157269997+PRVOFFCL@users.noreply.github.com>
2024-01-21 12:07:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/PRVOFFCL/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
+  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
 fi
 cd /THALAPATHY-FILTER-BOT 
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting PRVOFFCL...."
 python3 bot.py
